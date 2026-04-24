b9df5e1ffc2ba1eec24016579f6e4ee473ef7ffa
Bryan <brsibaja@estudiantec.cr>
2026-04-24 12:04:44 -0600

    Add Methods Comentario

--- Blog/src/logica/Comentario.java
+++ Blog/src/logica/Comentario.java
@@ -1,6 +1,62 @@
 package logica;
 
-public class Comentario {
+import java.time.LocalDateTime;
 
-	public void Comentario(String)
+public class Comentario {
+	
+	private static int consecutivo = 1;
+	private int codigo;
+	private LocalDateTime fechaCreacion;
+	private String email;
+	private String ip;
+	private String texto;
+	
+	public Comentario(String email, String ip, String texto) {
+		this.codigo = consecutivo;
+		this.email = email;
+		this.ip = ip;
+		this.texto = texto;
+		this.fechaCreacion = LocalDateTime.now();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Comentario: " + codigo +
+	            ", fechaCreacion=" + fechaCreacion +
+	            ", email='" + email + '\'' +
+	            ", ip='" + ip + '\'' +
+	            ", texto='" + texto + '\'';
+	}
 }
